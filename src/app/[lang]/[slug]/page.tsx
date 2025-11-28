82f5761f3a1175ad59375d8860a0dddaea71b6a3
tadmilin <tadeyes1@gmail.com>
2025-11-28 22:30:13 +0700

    big 3 เปลี่ยนภาษาภภ2/ หน้าเพจต่างงๆนน

--- src/app/[lang]/[slug]/page.tsx
+++ src/app/[lang]/[slug]/page.tsx
@@ -58,6 +58,7 @@ export default async function Page({ params: paramsPromise }: Args) {
 
   page = await queryPageBySlug({
     slug,
+    locale: lang,
   })
 
   // Remove this code once your website is seeded
@@ -86,10 +87,11 @@ export default async function Page({ params: paramsPromise }: Args) {
 }
 
 export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
-  const { lang: _lang, slug = 'home' } = await paramsPromise
+  const { lang, slug = 'home' } = await paramsPromise
 
   const page = await queryPageBySlug({
     slug,
+    locale: lang,
   })
 
   const metadata = generateMeta({ doc: page })
@@ -106,13 +108,14 @@ export async function generateMetadata({ params: paramsPromise }: Args): Promise
   }
 }
 
-const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
+const queryPageBySlug = cache(async ({ slug, locale }: { slug: string; locale?: string }) => {
   const { isEnabled: draft } = await draftMode()
 
   const payload = await getPayload({ config: configPromise })
 
   const result = await payload.find({
     collection: 'pages',
+    locale: locale || 'th',
     draft,
     limit: 1,
     pagination: false,
