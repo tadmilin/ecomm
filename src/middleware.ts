6feb3efc3c9df2d5eaf57efd3b85878541969eb2
tadmilin <tadeyes1@gmail.com>
2025-11-28 21:39:45 +0700

    big 3 เปลี่ยนภาษาภภ2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const languages = ['en', 'th']
+const languages = ['en', 'th', 'cn']
 const fallbackLng = 'en'
 
 export function middleware(request: NextRequest) {
