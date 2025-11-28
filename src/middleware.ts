947934d94fc72fc0656d9be27baea8c8f1488d87
tadmilin <tadeyes1@gmail.com>
2025-11-28 22:10:46 +0700

    big 3 เปลี่ยนภาษาภภ2/ หน้าเพจต่างงๆ

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 const languages = ['en', 'th', 'cn']
-const fallbackLng = 'en'
+const fallbackLng = 'th'
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
