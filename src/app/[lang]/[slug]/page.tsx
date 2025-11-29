d4916d0c42f3e39179906ff7f6022f87814da4c6
tadmilin <tadeyes1@gmail.com>
2025-11-29 11:02:28 +0700

    big 3 เปลี่ยนวิวัฒน์พัน1หฟก

--- src/app/[lang]/[slug]/page.tsx
+++ src/app/[lang]/[slug]/page.tsx
@@ -81,7 +81,7 @@ export default async function Page({ params: paramsPromise }: Args) {
       {draft && <LivePreviewListener />}
 
       <RenderHero {...hero} />
-      <RenderBlocks blocks={layout} />
+      <RenderBlocks blocks={layout} lang={lang} />
     </article>
   )
 }
