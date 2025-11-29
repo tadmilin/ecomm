1a0221e62d17d10e89cbc42b19cc07fbbbdcdaca
tadmilin <tadeyes1@gmail.com>
2025-11-29 12:03:57 +0700

    Set categories to dynamic for faster testing

--- src/app/[lang]/categories/page.tsx
+++ src/app/[lang]/categories/page.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { getTranslatedText } from '@/utilities/getTranslatedText'
 
-export const dynamic = 'force-static'
-export const revalidate = 600
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 type Args = {
   params: Promise<{
