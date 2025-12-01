e9b66b6fba3b44bcc13482d3d289f8f570614d3f
tadmilin <tadeyes1@gmail.com>
2025-12-01 16:11:27 +0700

    Change menu links to inline appearance to apply font size

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,7 +12,7 @@ export const HeaderNav: React.FC<{ data: HeaderType; lang: string }> = ({ data, 
   return (
     <nav className="flex gap-8 items-center text-lg font-semibold">
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} lang={lang} appearance="link" />
+        return <CMSLink key={i} {...link} lang={lang} appearance="inline" />
       })}
     </nav>
   )
