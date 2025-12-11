abeac34edf9e38d25d203e201475fac44af2978e
tadmilin <tadeyes1@gmail.com>
2025-12-12 01:54:03 +0700

    Fix header logo cache issue

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import type { Header } from '@/payload-types'
 
 export async function Header({ lang }: { lang?: string }) {
-  const headerData: Header = await getCachedGlobal('header', 1, lang)()
+  const headerData: Header = await getCachedGlobal('header', 1)()
 
   return <HeaderClient data={headerData} />
 }
